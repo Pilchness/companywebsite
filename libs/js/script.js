0d546d0414af29c578dd8c26cf2434ceb9c807a9
Richard Barnes <pilchness@gmail.com>
2020-12-30 19:46:55 -0500

    php edit

---
+++ libs/js/script.js
@@ -0,0 +1,33 @@
+class DatabaseQuery {
+    constructor(table) {
+      this.type = type;
+      this.table = table;
+      this.data = data;
+    }
+  
+    getData = async () => {
+      return new Promise((resolve, reject) => {
+        $.ajax({
+          type: 'POST',
+          url: 'libs/php/databaseFunctions.php',
+          dataType: 'json',
+          data: {
+            table: this.table
+          },
+          success: function (result) {
+            resolve(result);
+          },
+          error: function (error) {
+            reject(error);
+          }
+        });
+      });
+    };
+  }
+
+  const personnel = new DatabaseQuery(personnel)
+  personnel().then(response => console.log(response));
+
+  //type CREATE / REMOVE / UPDATE / DELETE
+  //table DEPT / LOCATION / PERSONNEL
+  //data
