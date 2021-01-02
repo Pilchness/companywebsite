f31d6b872a2d49cee80d68068d8c61bb6ed9e510
Richard Barnes <pilchness@gmail.com>
2021-01-02 12:34:31 -0500

    search function for main directory

--- libs/js/script.js
+++ libs/js/script.js
@@ -3,7 +3,8 @@ class DatabaseQuery {
     this.querytype = querytype;
   }
 
-  getData = async (search) => {
+  getData = async (search, param = 0) => {
+    console.log(search, param);
     return new Promise((resolve, reject) => {
       $.ajax({
         type: "POST",
@@ -12,6 +13,7 @@ class DatabaseQuery {
         data: {
           querytype: this.querytype,
           search: search,
+          param: param,
         },
         success: function (result) {
           resolve(result.data);
@@ -24,28 +26,41 @@ class DatabaseQuery {
   };
 }
 
+let visibleSearch = false;
 const personnelDirectoryQuery = new DatabaseQuery("personnel");
 const departmentDirectoryQuery = new DatabaseQuery("department");
+const locationDirectoryQuery = new DatabaseQuery("location");
+const idQuery = new DatabaseQuery("id");
 
 personnelDirectoryQuery.getData("all_personnel").then((response) => {
   //console.log(response);
   //returns list of all personnel and all of their data
 });
 
-personnelDirectoryQuery.getData(45).then((response) => {
-  //console.log(response[0]);
-  //returns data for staff memeber, passing staff ID
-});
+// personnelDirectoryQuery.getData(45).then((response) => {
+//   //console.log(response[0]);
+//   //returns data for staff memeber, passing staff ID
+// });
 
 departmentDirectoryQuery.getData("all").then((response) => {
   //console.log(response);
   //returns a list of departments with names and location IDs
 });
 
-departmentDirectoryQuery.getData(2).then((response) => {
+// departmentDirectoryQuery.getData(2).then((response) => {
+//   //console.log(response);
+//   //returns a list of staff in department, passing dept ID
+// });
+
+locationDirectoryQuery.getData("all").then((response) => {
   //console.log(response);
-  //returns a list of staff in department, passing dept ID
+  //returns a list of locations with names and location IDs
 });
+
+// locationDirectoryQuery.getData(2).then((response) => {
+//   //console.log(response);
+//   //returns a list of staff in department, passing dept ID
+// });
 
 //get all
 //search by name
@@ -60,15 +75,17 @@ const loadDashboard = () => {
   console.log("loading dashboard");
 };
 
-const mainDirectory = async (search) => {
+const mainDirectory = async (search, department) => {
+  console.log(search, department);
   $("#main-directory").empty();
-  personnelDirectoryQuery.getData(search).then((response) => {
+  personnelDirectoryQuery.getData(search, department).then((response) => {
+    console.log(response);
     response.forEach((person) => {
       $("#main-directory")
         .append(`<div class="card border-dark mb-1" style="max-width: 100%;">
         <div class="card-header">${person.firstName} ${person.lastName}</div>
         <div class="card-body text-dark">
-        <img src='images/staffpics/staffphoto_id_${person.id}.jpg' width='50px' height='50px'/>
+        <img class="headshot" id="${person.id}" src='images/staffpics/staffphoto_id${person.id}.jpg' width='50px' height='50px'/>
           <ul style="margin-left: 5px; margin-top: 5px">
           <li class="person-card-text"><b>Dept:</b> ${person.department}</li>
           <li class="person-card-text"><b>Location:</b> ${person.location}</li>
@@ -77,6 +94,7 @@ const mainDirectory = async (search) => {
         </div>
       </div>`);
     });
+    document.getElementById("main-content").scrollIntoView();
   });
 };
 
@@ -87,20 +105,120 @@ const departmentList = async () => {
         .append(`<div class="card border-dark mb-1" style="max-width: 100%;">
         <div class="card-header">${department.name}</div>
         <div class="dept-card-body text-dark">
-          <ul id="personnel-dept-${department.id}" class="dept-list">
+          <ul id="personnel-dept-${department.id}" class="dept-section">
           </ul>
         </div>
       </div>`);
       departmentDirectoryQuery.getData(department.id).then((response) => {
         response.forEach((departmentMember) => {
           $(`#personnel-dept-${department.id}`).append(
-            `<div class="dept-photo"><img src='images/staffpics/staffphoto_id_${departmentMember.id}.jpg' width="30px" height="30px"/>
-            <a class="person-card-text">${departmentMember.firstName} ${departmentMember.lastName}</a></div>`
+            `<div class="dept-photo headshot" id="${departmentMember.id}"><img src='images/staffpics/staffphoto_id${departmentMember.id}.jpg' width="30px" height="30px"/>
+            <span class="person-card-text">${departmentMember.firstName} ${departmentMember.lastName}</span></div>`
           );
         });
+        document.getElementById("main-content").scrollIntoView();
       });
     });
   });
+};
+
+const locationList = async () => {
+  locationDirectoryQuery.getData("all").then((response) => {
+    response.forEach((location) => {
+      $("#location-list")
+        .append(`<div class="card border-dark mb-1" style="max-width: 100%;">
+        <div class="card-header">${location.name}</div>
+        <div class="dept-card-body text-dark">
+          <ul id="personnel-dept-${location.id}" class="location-section">
+          </ul>
+        </div>
+      </div>`);
+      locationDirectoryQuery.getData(location.id).then((response) => {
+        response.forEach((locationMember) => {
+          $(`#personnel-dept-${location.id}`).append(
+            `<div class="location-photo headshot" id="${locationMember.id}"><img src='images/staffpics/staffphoto_id${locationMember.id}.jpg' width="30px" height="30px"/>
+            <span class="person-card-text">${locationMember.firstName} ${locationMember.lastName} (${locationMember.department})</span></div>`
+          );
+        });
+        document.getElementById("main-content").scrollIntoView();
+      });
+    });
+  });
+};
+
+const toggleSearchBar = async () => {
+  if (!visibleSearch) {
+    $(".directory-content").css("margin-top", "210px");
+    $("#main-content-header").append(`
+    <div
+  id="search-bar"
+  style="margin-top: 3px; width: 100%; padding-right: 3px; display: flex"
+>
+  <form class="form-inline" style="display: flex;">
+    <input
+    style="flex 1"
+      id="name-search"
+      class="form-control mr-sm-1"
+      type="search"
+      spellcheck="false"
+      placeholder="Search"
+      aria-label="Search"
+    />
+    <select style="flex 1; border-radius: 5px" class="custom-select" id="department-selector">
+      <option id="0" selected>All Departments</option>
+    </select>
+  </form>
+</div>`);
+    departmentDirectoryQuery.getData("all").then((response) => {
+      response.forEach((department) => {
+        let departmentTextString = department.name;
+        let maxLength = 14;
+        if (departmentTextString.length > maxLength) {
+          departmentTextString =
+            departmentTextString.substr(0, maxLength) + "...";
+        }
+        $("#department-selector").append(
+          `<option id="${department.id}">${departmentTextString}</option>`
+        );
+      });
+    });
+
+    // $("#department-selector").on("change", function (e) {
+    //   let selectedDepartment = $("#department-selector option:selected").attr(
+    //     "id"
+    //   );
+    //   departmentDirectoryQuery.getData(selectedDepartment).then((response) => {
+    //     console.log(response);
+
+    //   });
+    // });
+
+    const getNewData = () => {
+      if ($("#person-file").length) {
+        $("#person-file").remove();
+        $("#main-content").html(
+          `<ul id="main-directory" class="directory-content"></ul>`
+        );
+        $(".directory-content").css("margin-top", "210px");
+      }
+      mainDirectory(
+        $("#name-search").val(),
+        $("#department-selector option:selected").attr("id")
+      );
+    };
+
+    $("#name-search").on("keyup", function () {
+      getNewData();
+    });
+    $("#department-selector").on("change", function () {
+      getNewData();
+    });
+    visibleSearch = true;
+  } else {
+    $("#search-bar").remove();
+    $(".directory-content").css("margin-top", "170px");
+    visibleSearch = false;
+  }
 };
 
 const loadPersonnelPage = () => {
@@ -108,51 +226,37 @@ const loadPersonnelPage = () => {
       <div id="personnel-button-container" class="nav nav-tabs">
       <button id="directory" class="personnel-button"><h3>Directory</h3></button>
       <button id="departments" class="personnel-button"><h3>Teams</h3></button>
-      <button id="chart" class="personnel-button"><h3>Chart</h3></button>
+      <button id="locations" class="personnel-button"><h3>Locations</h3></button>
       <img id="search-icon" src='images/icons/search.png'alt='search database' width='20px' height='20px'/>
       </div>`);
   $("#search-icon").on("click", function () {
-    $("#main-content-header").append(`
-        <div style="margin-top: 3px; width: 100%; padding-right: 3px; display: flex">
-        <form style="flex: 2" class="form-inline">
-        <input id="name-search" class="form-control mr-sm-1" type="search" placeholder="Search" aria-label="Search">
-        </form>
-        <div class="dropdown" style="flex: 1">
-  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-    All Departments
-  </button>
-  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-    <a class="dropdown-item" href="#">Action</a>
-    <a class="dropdown-item" href="#">Another action</a>
-    <a class="dropdown-item" href="#">Something else here</a>
-  </div>
-</div>
-        
-        
-        </div>`);
-    $("#main-directory").css("margin-top", "210px");
-    $("#name-search").on("keyup", function () {
-      console.log($(this).val());
-      mainDirectory($(this).val());
-    });
+    toggleSearchBar();
   });
+
   const loadPersonnelTab = (tab) => {
     switch (tab) {
       case "directory":
         $("#directory").focus();
-        $("#main-content").html(`<ul id="main-directory"></ul>`);
+        $("#main-content").html(
+          `<ul id="main-directory" class="directory-content"></ul>`
+        );
         mainDirectory("all_personnel");
         break;
 
       case "departments":
         $("#departments").focus();
-        $("#main-content").html(`<ul id="department-list"></ul>`);
+        $("#main-content").html(
+          `<ul id="department-list" class="directory-content"></ul>`
+        );
         departmentList();
         break;
 
-      case "chart":
-        $("#chart").focus();
-        $("#main-content").html(`<h3 style="color: white">Chart</h3>`);
+      case "locations":
+        $("#locations").focus();
+        $("#main-content").html(
+          `<ul id="location-list" class="directory-content"></ul>`
+        );
+        locationList();
         break;
 
       default:
@@ -164,6 +268,30 @@ const loadPersonnelPage = () => {
 
   $(".personnel-button").on("click", function () {
     loadPersonnelTab($(this).attr("id"));
+  });
+};
+
+const showPersonFile = async (id) => {
+  idQuery.getData(id).then((response) => {
+    let person = response[0];
+    $("#main-content").scrollTop(0);
+    $("#main-content").html(
+      `<div class="card directory-content" id="person-file">
+  <img class="card-img-top" src='images/staffpics/staffphoto_id${person.id}.jpg' alt="${person.firstName} ${person.lastName}">
+  <div id="person-file-body">
+    <h2 class="card-title">${person.firstName} ${person.lastName}</h2>
+      <ul id="person-file-info">
+      <li>Location: ${person.location}</li>
+      <li>Department: ${person.department}</li>
+      <li>Email: ${person.email}</li>
+      </ul>
+   
+  </div>
+ 
+</div>`
+    );
+    $("#search-bar").empty();
+    document.getElementById("main-content").scrollIntoView();
   });
 };
 
@@ -180,5 +308,9 @@ $(document).ready(function () {
   $(".icon").on("click", function () {
     $("#page-title").text($(this).attr("value"));
     loadPage($(this).attr("id"));
+  });
+  $(document).on("click", ".headshot", function () {
+    console.log($(this).attr("id"));
+    showPersonFile($(this).attr("id"));
   });
 });
--- libs/php/databaseFunctions.php
+++ libs/php/databaseFunctions.php
@@ -1,104 +1,141 @@
 <?php
 
-	// remove next two lines for production
-	
-	ini_set('display_errors', 'On');
-	error_reporting(E_ALL);
+// remove next two lines for production
 
-	$executionStartTime = microtime(true);
+ini_set('display_errors', 'On');
+error_reporting(E_ALL);
 
-	include("config.php");
+$executionStartTime = microtime(true);
 
-	header('Content-Type: application/json; charset=UTF-8');
+include("config.php");
 
-	$conn = new mysqli($cd_host, $cd_user, $cd_password, $cd_dbname, $cd_port, $cd_socket);
+header('Content-Type: application/json; charset=UTF-8');
 
-	if (mysqli_connect_errno()) {
-		
-		$output['status']['code'] = "300";
-		$output['status']['name'] = "failure";
-		$output['status']['description'] = "database unavailable";
-		$output['status']['returnedIn'] = (microtime(true) - $executionStartTime) / 1000 . " ms";
-		$output['data'] = [];
+$conn = new mysqli($cd_host, $cd_user, $cd_password, $cd_dbname, $cd_port, $cd_socket);
 
-		mysqli_close($conn);
+if (mysqli_connect_errno()) {
 
-		echo json_encode($output);
+	$output['status']['code'] = "300";
+	$output['status']['name'] = "failure";
+	$output['status']['description'] = "database unavailable";
+	$output['status']['returnedIn'] = (microtime(true) - $executionStartTime) / 1000 . " ms";
+	$output['data'] = [];
 
-		exit;
+	mysqli_close($conn);
 
-	}	
+	echo json_encode($output);
 
-	switch ($_POST['querytype']) {
-		case 'personnel':
-			if($_POST['search'] === 'all_personnel' || $_POST['search'] === '') {
-				
-					$query = 'SELECT p.id, p.lastName, p.firstName, p.jobTitle, p.email, d.name as department, l.name as location 
+	exit;
+}
+
+switch ($_POST['querytype']) {
+	case 'personnel':
+
+		if ($_POST['search'] === 'all_personnel' || $_POST['search'] === '') {
+			if ($_POST['param'] == 0) {
+
+				$query = 'SELECT p.id, p.lastName, p.firstName, p.jobTitle, p.email, d.name as department, l.name as location 
 								FROM personnel p 
 								LEFT JOIN department d ON (d.id = p.departmentID) 
 								LEFT JOIN location l ON (l.id = d.locationID) 
-								ORDER BY p.lastName, p.firstName, d.name, l.name'; 
-				
-			} else {
-					$query = 'SELECT p.id, p.lastName, p.firstName, p.jobTitle, p.email, d.name as department, l.name as location 
-								FROM personnel p
-								LEFT JOIN department d ON (d.id = p.departmentID) 
-								LEFT JOIN location l ON (l.id = d.locationID) 
-								WHERE p.firstName LIKE "%' . $_POST['search'] . '%"
-								OR p.lastName LIKE "%' . $_POST['search'] . '%"
-								OR p.email LIKE "%' . $_POST['search'] . '%"
 								ORDER BY p.lastName, p.firstName, d.name, l.name';
+			} else {
+				$query = 'SELECT p.id, p.lastName, p.firstName, p.jobTitle, p.email, d.name as department, l.name as location 
+				FROM personnel p
+				LEFT JOIN department d ON (d.id = p.departmentID) 
+				LEFT JOIN location l ON (l.id = d.locationID) 
+				WHERE d.id = ' . $_POST['param'] . '
+				ORDER BY p.lastName, p.firstName, d.name, l.name';
 			}
-			break;
+		} else {
+			if ($_POST['param'] == 0) {
+				$query = 'SELECT p.id, p.lastName, p.firstName, p.jobTitle, p.email, d.name as department, l.name as location 
+				FROM personnel p
+				LEFT JOIN department d ON (d.id = p.departmentID) 
+				LEFT JOIN location l ON (l.id = d.locationID) 
+				WHERE p.firstName LIKE "%' . $_POST['search'] . '%"
+				OR p.lastName LIKE "%' . $_POST['search'] . '%"
+				OR p.email LIKE "%' . $_POST['search'] . '%"
+				ORDER BY p.lastName, p.firstName, d.name, l.name';
+			} else {
+				$query = 'SELECT p.id, p.lastName, p.firstName, p.jobTitle, p.email, d.name as department, l.name as location 
+									FROM personnel p
+									LEFT JOIN department d ON (d.id = p.departmentID) 
+									LEFT JOIN location l ON (l.id = d.locationID) 
+									WHERE d.id = ' . $_POST['param'] . '
+									AND (p.firstName LIKE "%' . $_POST['search'] . '%"
+									OR p.lastName LIKE "%' . $_POST['search'] . '%"
+									OR p.email LIKE "%' . $_POST['search'] . '%")
+									ORDER BY p.lastName, p.firstName, d.name, l.name';
+			}
+		}
+		break;
 
-			case 'department':
-				if($_POST['search'] === 'all') {
-					$query = 'SELECT id, name, locationID 
+	case 'department':
+		if ($_POST['search'] === 'all') {
+			$query = 'SELECT id, name, locationID 
 					FROM department';
-				} else {
-					$query = 'SELECT * FROM personnel p
-					WHERE p.departmentID = ' . $_POST['search']; 
-				};
-			break;
-		
-		default:
-			echo ('querytype error');
-			break;
-	}
+		} else {
+			$query = 'SELECT * FROM personnel p
+					WHERE p.departmentID = ' . $_POST['search'];
+		};
+		break;
 
-	$result = $conn->query($query);
-	
-	if (!$result) {
+	case 'location':
+		if ($_POST['search'] === 'all') {
+			$query = 'SELECT id, name
+					FROM location';
+		} else {
+			$query = 'SELECT p.id, p.lastName, p.firstName, d.name as department, l.name as location 
+					FROM personnel p
+					LEFT JOIN department d ON (d.id = p.departmentID) 
+					LEFT JOIN location l ON (l.id = d.locationID) 
+					WHERE l.id = ' . $_POST['search'];
+		};
+		break;
 
-		$output['status']['code'] = "400";
-		$output['status']['name'] = "executed";
-		$output['status']['description'] = "query failed";	
-		$output['data'] = [];
+	case 'id':
+		$query = 'SELECT p.id, p.lastName, p.firstName, p.jobTitle, p.email, d.name as department, l.name as location 
+				FROM personnel p 
+				LEFT JOIN department d ON (d.id = p.departmentID) 
+				LEFT JOIN location l ON (l.id = d.locationID)
+				WHERE p.id =' . $_POST['search'];
+		break;
 
-		mysqli_close($conn);
+	default:
+		echo ('querytype error');
+		break;
+}
 
-		echo json_encode($output); 
+$result = $conn->query($query);
 
-		exit;
+if (!$result) {
 
-	}
-   
-   	$data = [];
+	$output['status']['code'] = "400";
+	$output['status']['name'] = "executed";
+	$output['status']['description'] = "query failed";
+	$output['data'] = [];
 
-	while ($row = mysqli_fetch_assoc($result)) {
-
-		array_push($data, $row);
-
-	}
-
-	$output['status']['code'] = "200";
-	$output['status']['name'] = "ok";
-	$output['status']['description'] = "success";
-	$output['status']['returnedIn'] = (microtime(true) - $executionStartTime) / 1000 . " ms";
-	$output['data'] = $data;
-	
 	mysqli_close($conn);
 
-	echo json_encode($output); 
+	echo json_encode($output);
 
-?>
+	exit;
+}
+
+$data = [];
+
+while ($row = mysqli_fetch_assoc($result)) {
+
+	array_push($data, $row);
+}
+
+$output['status']['code'] = "200";
+$output['status']['name'] = "ok";
+$output['status']['description'] = "success";
+$output['status']['returnedIn'] = (microtime(true) - $executionStartTime) / 1000 . " ms";
+$output['data'] = $data;
+
+mysqli_close($conn);
+
+echo json_encode($output);
